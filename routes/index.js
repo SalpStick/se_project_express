ec10bc679d99d94acd87bb89d10ccacc7b7811b9
SalpStick <43050248+SalpStick@users.noreply.github.com>
2024-08-07 14:42:03 -0400

    Fix/ index.js routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const userRouter = require("./users");
 const clothingItemRouter = require("./clothingItems");
 const { login, createUser } = require("../controllers/users");
+const { auth } = require("../middlewares/auth");
 
 const { ERROR_CODES, ERROR_MESSAGES } = require("../utils/errors");
 
