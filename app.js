41e66fc3b4823af3dcbbccc1aea13fd7adc11ae7
SalpStick <43050248+SalpStick@users.noreply.github.com>
2024-08-07 10:44:37 -0400

    Sprint 13 initial commit

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
-app.use("/", indexRouter);
-
 app.use(cors());
+
+app.use("/", indexRouter);
 
 
 app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`);});
