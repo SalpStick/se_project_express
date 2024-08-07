56c7b1a8aac5d053bf3718776f9d43aad7f12d3f
SalpStick <43050248+SalpStick@users.noreply.github.com>
2024-08-07 14:59:40 -0400

    Fix/ index.js routes

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
 const router = require("express").Router();
 const userRouter = require("./users");
 const clothingItemRouter = require("./clothingItems");
-const { login, createUser } = require("../controllers/users");
+const { login, createUser, getUser } = require("../controllers/users");
 const { auth} = require("../middlewares/auth");
 
 const { ERROR_CODES, ERROR_MESSAGES } = require("../utils/errors");
 
-router.post("/signin",auth, login);
+router.post("/signin", auth, login);
 router.post("/signup", auth, createUser);
 
 router.use("/users", userRouter);
 router.use("/items", clothingItemRouter);
 
-router.get("/users/me", auth, getCurrentUser);
+router.get("/users/me", auth, getUser);
 
 
 router.use((req, res) => {
