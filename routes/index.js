701e6d9d0394eefe8bc74512ff69f2bb197606b6
SalpStick <43050248+SalpStick@users.noreply.github.com>
2024-08-07 14:47:30 -0400

    Fix/ index.js routes

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,18 @@ const router = require("express").Router();
 const userRouter = require("./users");
 const clothingItemRouter = require("./clothingItems");
 const { getUser, login, createUser } = require("../controllers/users");
-const { auth } = require("../middlewares/auth");
+const { authMiddleware } = require("../middlewares/auth");
 
 const { ERROR_CODES, ERROR_MESSAGES } = require("../utils/errors");
 
 router.post("/signin", login);
 router.post("/signup", createUser);
 
-router.use("/users", auth, userRouter);
-router.use("/items", auth, clothingItemRouter);
+router.use("/users", authMiddleware, userRouter);
+router.use("/items", authMiddleware, clothingItemRouter);
 router.post("/signin", login);
 router.post("/signup", createUser);
-router.get("/users/me", auth, getUser);
+router.get("/users/me", authMiddleware, getUser);
 
 router.use((req, res) => {
   res.status(ERROR_CODES.NOT_FOUND).send({ message: ERROR_MESSAGES.NOT_FOUND });
