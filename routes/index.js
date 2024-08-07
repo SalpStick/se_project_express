9bc5bbdb84638986922217682d476bd21adb8d24
SalpStick <43050248+SalpStick@users.noreply.github.com>
2024-08-07 14:39:02 -0400

    Fix/ index.js routes

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,11 @@ const { ERROR_CODES, ERROR_MESSAGES } = require("../utils/errors");
 router.post("/signin", login);
 router.post("/signup", createUser);
 
-router.use("/users", userRouter);
-router.use("/items", clothingItemRouter);
+router.use("/users", auth, userRouter);
+router.use("/items", auth, clothingItemRouter);
+router.post("/signin", login);
+router.post("/signup", createUser);
+router.get("/users/me", auth, getUser);
 
 router.use((req, res) => {
   res.status(ERROR_CODES.NOT_FOUND).send({ message: ERROR_MESSAGES.NOT_FOUND });
