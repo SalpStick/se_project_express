d1d3bde4d7a92701f056f7afcb99c2b6ae8ddb50
SalpStick <43050248+SalpStick@users.noreply.github.com>
2024-08-08 11:37:13 -0400

    test

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer")) {
-    return res.status(400).send({ message: "Authorization required 1st" });
+    return res.status(400).send({ message: `${authorization} Authorization required 1st` });
   }
 
   const token = authorization.replace("Bearer ", "");
@@ -14,10 +14,10 @@ const auth = (req, res, next) => {
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return res.status(400).send({ message: "Authorization required 2nd" });
+    return res.status(400).send({ message: `Authorization required 2nd` });
   }
 
-  console.log(payload, "authorization from auth middleware");
+  console.log(payload, `authorization from auth middleware`);
   req.user = payload;
   return next();
 };
