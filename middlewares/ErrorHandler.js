a116b630d253edc903039a1ee1b8d6703263a90f
SalpStick <43050248+SalpStick@users.noreply.github.com>
2024-10-10 13:25:03 -0400

    fix/ lintrc errors

--- middlewares/ErrorHandler.js
+++ middlewares/ErrorHandler.js
@@ -1,11 +1,12 @@
 const { ERROR_CODES, ERROR_MESSAGES } = require('../utils/errors');
 
-const errorHandler = (err, req, res) => {
+const errorHandler = (err, req, res, next) => {
 
   const status = err.statusCode || ERROR_CODES.SERVER_ERROR;
   const message = err.message || ERROR_MESSAGES.SERVER_ERROR;
-
-  res.status(status).send({ message });
+  
+  console.error(status + ' ' + message);
+  next(err);
 };
 
 module.exports = errorHandler;
